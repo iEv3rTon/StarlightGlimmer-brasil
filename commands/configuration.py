69f4bfdd99b05dfd1f26ed737a49ea7082b3f805
George <4011593-BrickGrass@users.noreply.gitlab.com>
2020-01-21 11:10:41 +0000

    Update commands/configuration.py

--- commands/configuration.py
+++ commands/configuration.py
@@ -58,13 +58,13 @@ class Configuration(commands.Cog):
         # Guild does not have autoscan enabled, enable it
         if sql.guild_is_autoscan(ctx.guild.id) == False:
             sql.guild_update(ctx.guild.id, autoscan=1)
-            print(sql.guild_get_by_id(ctx.guild.id))
+            print(dir(sql.guild_get_by_id(ctx.guild.id)))
             log.info("Autoscan enabled for {0.name} (GID: {0.id})".format(ctx.guild))
             await ctx.send(ctx.s("configuration.autoscan_enabled"))
         # Guild has autoscan enabled, disable it
         else:
             sql.guild_update(ctx.guild.id, autoscan=0)
-            print(sql.guild_get_by_id(ctx.guild.id))
+            print(dir(sql.guild_get_by_id(ctx.guild.id)))
             log.info("Autoscan disabled for {0.name} (GID: {0.id})".format(ctx.guild))
             await ctx.send(ctx.s("configuration.autoscan_disabled"))
 
